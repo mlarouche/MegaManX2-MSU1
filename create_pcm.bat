f42857ed5cef9c8e5be5db5e9e958e90995f8de5
Michaël Larouche <michael.larouche@gmail.com>
2015-12-19 11:35:55 -0500

    Commit create_pcm before I loose it again

--- create_pcm.bat
+++ create_pcm.bat
@@ -2,3 +2,34 @@
 
 del *.pcm
 
+wav2msu -o mmx2_msu1-1.pcm mmx2_morph_moth.wav
+wav2msu -o mmx2_msu1-2.pcm -l 225618 mmx2_magna_centipede.wav
+wav2msu -o mmx2_msu1-3.pcm -l 253415 mmx2_wire_sponge.wav
+wav2msu -o mmx2_msu1-4.pcm -l 332284 mmx2_bubble_crab.wav
+wav2msu -o mmx2_msu1-5.pcm mmx2_flame_stag.wav
+wav2msu -o mmx2_msu1-6.pcm mmx2_overdrive_ostrich.wav
+wav2msu -o mmx2_msu1-7.pcm mmx2_wheel_gator.wav
+wav2msu -o mmx2_msu1-8.pcm -l 154965 mmx2_opening_stage.wav
+wav2msu -o mmx2_msu1-9.pcm mmx2_boss_fight.wav
+wav2msu -o mmx2_msu1-10.pcm -l 561181 mmx2_another_boss2.wav
+wav2msu -o mmx2_msu1-11.pcm mmx2_title.wav
+wav2msu -o mmx2_msu1-12.pcm mmx2_stage_select1.wav
+wav2msu -o mmx2_msu1-13.pcm -l 228080 mmx2_password.wav
+wav2msu -o mmx2_msu1-14.pcm mmx2_crystal_snail.wav
+wav2msu -o mmx2_msu1-15.pcm mmx2_stage_start.wav
+wav2msu -o mmx2_msu1-16.pcm -l 220584 mmx2_zero.wav
+wav2msu -o mmx2_msu1-17.pcm mmx2_xhunter_stage2.wav
+wav2msu -o mmx2_msu1-18.pcm -l 139551 mmx2_stage_select2.wav
+wav2msu -o mmx2_msu1-19.pcm mmx2_stage_clear.wav
+wav2msu -o mmx2_msu1-20.pcm mmx2_opening.wav
+wav2msu -o mmx2_msu1-21.pcm -l 134697 mmx2_another_boss1.wav
+wav2msu -o mmx2_msu1-22.pcm -l 126199 mmx2_boss_intro.wav
+wav2msu -o mmx2_msu1-23.pcm -l 39228 mmx2_get_a_weapon.wav
+wav2msu -o mmx2_msu1-24.pcm -l 171602 mmx2_xhunter_stage1.wav
+wav2msu -o mmx2_msu1-26.pcm -l 154965 mmx2_opening_stage.wav
+wav2msu -o mmx2_msu1-27.pcm mmx2_demo.wav
+wav2msu -o mmx2_msu1-28.pcm mmx2_sigma1.wav
+wav2msu -o mmx2_msu1-29.pcm mmx2_sigma2.wav
+wav2msu -o mmx2_msu1-30.pcm mmx2_ending.wav
+wav2msu -o mmx2_msu1-31.pcm mmx2_staff_roll.wav
+wav2msu -o mmx2_msu1-32.pcm -l 541498 mmx2_laboratory.wav
